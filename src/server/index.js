a2fad105324c66c60b92bc26210b0ae65ace20b1
Linus Farlock <linus.farlock@logon-consulting.de>
2018-05-16 21:35:04 +0200

    added express backend

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const os = require("os");
 const app = express();
+
 app.use(express.static("dist"));
-app.get("/api/getUsername", (req, res) =>
+app.get("/api/products", (req, res) =>
   res.send({ username: os.userInfo().username })
 );
-app.listen(8080, () => console.log("Listening on port 8080!"));
+app.listen(5001, () => console.log("Listening on port 5001!"));
